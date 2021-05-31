67e56d2d00e45db560667e3a88ef93cb205e1b62
Super-Web112 <74626440+Super-Web112@users.noreply.github.com>
2021-05-31 09:53:20 +0900

    init_commit

---
+++ js/scripts.js
@@ -0,0 +1,45 @@
+﻿$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+
+    if (scroll > 450) {
+        $(".step1").addClass("step2");
+        $(".inner-step1").addClass("fadeOut");
+    } else {
+        $(".step1").removeClass("step2");
+        $(".inner-step1").removeClass("fadeOut");
+
+    }
+});
+
+
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+    if (scroll > 900) {
+		$(".inner-step2").addClass("fadeIn");
+        $(".step1").addClass("step3");
+        
+    } else {
+		$(".inner-step2").removeClass("fadeIn");
+        $(".step1").removeClass("step3");
+    }
+
+});
+
+
+
+
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+    if (scroll > 1250) {
+
+		$(".inner-step3").addClass("fadeIn");
+
+        $(".step1").addClass("step4");
+        
+    } else {
+		$(".inner-step3").removeClass("fadeIn");
+        $(".step1").removeClass("step4");
+    }
+
+});
+
